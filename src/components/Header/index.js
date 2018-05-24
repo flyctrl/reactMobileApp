d7751741f7786a74646d8e575c8ad3b0d41f3159
sunsq01 <sunsq01@jcgroup.com.cn>
2018-05-24 19:31:36 +0800

    添加全局细线

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -15,13 +15,12 @@ class Header extends Component {
       {isIphoneX ? <div className={style['fix-iphoneX-top']}/> : null}
       <NavBar
         mode='light'
-        className={style['nav-bar']}
+        className={`${style['nav-bar']} ${noLine ? '' : 'my-bottom-border'}`}
         leftContent={[
           leftIcon && <span className='leftBox' key={1} ><NewIcon type={leftIcon} onClick={leftClick1} className='leftIcon' /></span>, leftTitle1 && <span key={2} className='usr-hdleft-title-first' onClick={leftClick1} >{leftTitle1}</span>, leftTitle2 && <span key={3} onClick={leftClick2} className='usr-hdleft-title-second'>{leftTitle2}</span>]}
         rightContent={[rightIcon && <span key={4} className='rightBox' ><NewIcon type={rightIcon} onClick={rightClick} className='rightIcon'/></span>, rightTitle && <span key={5} onClick={rightClick} className='usr-hdright-title'>{rightTitle}</span>]}
       >{searchTitle ? <SearchBar onFocus={onSearchFocus} onCancel={onSearchCancel} ref={ (ref) => { autoFocusInst ? this.autoFocusInst = ref : null }} cancelText={cancelText} key={6} onSubmit={onSearchSubmit} className='search' placeholder={searchTitle} maxLength={8}/> : <span key={7} onClick={titleClick} className='title'>{title}
         </span>}</NavBar>
-      { noLine ? null : <div style={{ backgroundColor: 'rgba(0,0,0,.09)', height: '1px' }}/> }
     </div>
   }
 }
